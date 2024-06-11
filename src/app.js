bd70f255aad80d55475439aa8f8a58b63138b13b
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-06-11 12:20:44 +0200

    app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let accion = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+        "before the class!",
+        "when I was sleeping!",
+        "while I was exercising!",
+        "during my lunch!",
+        "while I was praying!"
+    ];
+    
+    let randIndex = (array) => Math.floor(Math.random() * array.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    function generarExcusa(who, accion, what, when) {
+        let randWho = randIndex(who);
+        let randAccion = randIndex(accion);
+        let randWhat = randIndex(what);
+        let randWhen = randIndex(when);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        let excusa = `${who[randWho]} ${accion[randAccion]} ${what[randWhat]} ${when[randWhen]}`
+        document.getElementById("excusa").innerHTML = excusa;
+        return excusa;
+    }
+    generarExcusa(who, accion, what, when);
+}
